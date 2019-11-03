646ac3e8a0f2b3f885187c6c5a559a450a080552
Ivan Pianetti <ivanpianetti23@gmail.com>
2019-11-03 15:44:05 -0300

    Variables de ambiente

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,56 @@
 import 'reflect-metadata';
-import {createConnection} from 'typeorm';
+import {createConnection, getConnectionOptions} from 'typeorm';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as helmet from 'helmet';
 import * as cors from 'cors';
 import {RegisterRoutes} from './router/routes';
+import {MongoConnectionOptions} from 'typeorm/driver/mongodb/MongoConnectionOptions';
 const swaggerUi = require('swagger-ui-express');
 
-const port = process.env.PORT || 3000;
+// Obtenemos variables de .env
+const port = process.env.APP_PORT || 3000;
+const {DB_HOST, DB_PORT, DB_NAME} = process.env;
 
-//Connects to the Database -> then starts the express
-createConnection()
-  .then(async connection => {
-    // Create a new express application instance
-    const app = express();
+if (!DB_HOST || !DB_PORT || !DB_NAME) {
+  throw new Error("Variables de entorno no encontradas. Verifique archivo '.env'.");
+}
 
-    // Call midlewares
-    app.use(cors());
-    app.use(helmet());
-    app.use(bodyParser.json());
+// read connection options from ormconfig file (or ENV variables)
+// Leemos conexión de `ormconfig.json` y completamos datos
+getConnectionOptions().then((connectionOptions: MongoConnectionOptions) => {
+  // Completamos configuración según environment
+  Object.assign(connectionOptions, {host: DB_HOST, port: DB_PORT, database: DB_NAME});
 
-    // Cargamos todas las rutas generadas por TSOA
-    RegisterRoutes(app);
+  // Creamos conexión y luego iniciamos express
+  createConnection(connectionOptions)
+    .then(async connection => {
+      // Creamos instancia express
+      const app = express();
 
-    // Cargamos la documentación Swagger generada
-    try {
-      const swaggerDoc = require('../swagger.json');
-      // Exponemos documentación SwaggerUI en la siguiente URL
-      app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-    } catch (error) {
-      console.error('Error al cargar documentación Swagger: ', error);
-    }
+      // Midlewares
+      app.use(cors());
+      app.use(helmet());
+      app.use(bodyParser.json());
 
-    app.listen(port, () => {
-      console.log(`🚀 Server started: http://localhost:${port}/doc`);
+      // Cargamos todas las rutas generadas por TSOA
+      RegisterRoutes(app);
+
+      // Cargamos la documentación Swagger generada
+      try {
+        const swaggerDoc = require('../swagger.json');
+        // Exponemos documentación SwaggerUI en la siguiente URL
+        app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+      } catch (error) {
+        console.error('Error al cargar documentación Swagger: ', error);
+      }
+
+      app.listen(port, () => {
+        console.log(`🚀 Server started: http://localhost:${port}/doc`);
+      });
+    })
+    .catch(error => {
+      console.log('🐛 Server Error!');
+      console.error(error);
     });
-  })
-  .catch(error => console.log(error));
+});
