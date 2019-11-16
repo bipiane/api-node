71ab94abe3c5d00b70fb21ff7800de372b48efae
Ivan Pianetti <ivanpianetti23@gmail.com>
2019-11-16 17:57:52 -0300

    Definimos tipo en conexión TypeORM

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,19 +1,22 @@
+import {MongoConnectionOptions} from 'typeorm/driver/mongodb/MongoConnectionOptions';
+
 // Cargamos tanto las variables del archivo `.envrc` para desarrollo local
 // como las variables de `docker-compose.yml` para docker
-const {DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD} = process.env;
+let {DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD} = process.env;
 
 if (!DB_HOST || !DB_PORT || !DB_NAME || !DB_USER || !DB_PASSWORD) {
   throw new Error("Variables de entorno no encontradas. Verifique archivo '.envrc'.");
 }
 
-module.exports = {
+const conn: MongoConnectionOptions = {
   type: 'mongodb',
   host: DB_HOST,
-  port: DB_PORT,
+  port: Number(DB_PORT),
   database: DB_NAME,
   username: DB_USER,
   password: DB_PASSWORD,
   synchronize: true,
+  useNewUrlParser: true,
   useUnifiedTopology: true,
   logging: true,
   entities: ['src/entity/**/*.ts'],
@@ -25,3 +28,5 @@ module.exports = {
     subscribersDir: 'src/subscriber',
   },
 };
+
+module.exports = conn;
