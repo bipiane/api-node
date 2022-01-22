6f7b906fdc4433958af79bedd4ed7e4bba180d51
Ivan Pianetti <ivanpianetti23@gmail.com>
2022-01-22 20:59:06 -0300

    Added git version

---
+++ git.version.ts
@@ -0,0 +1,30 @@
+import {writeFileSync} from 'fs';
+import {promisify} from 'util';
+import * as child from 'child_process';
+const exec = promisify(child.exec);
+const moment = require('moment');
+
+/**
+ * Creates App version file
+ */
+async function createVersionsFile(filename: string) {
+  const revision = (await exec('git rev-parse --short HEAD')).stdout.toString().trim();
+  const branch = (await exec('git rev-parse --abbrev-ref HEAD')).stdout.toString().trim();
+
+  console.log(` 🚀 Version: '${process.env.npm_package_version}', revision: '${revision}', branch: '${branch}'`);
+
+  const content = `
+      /* tslint:disable */
+      // This file was auto-generated with git.version.ts script       
+      export const versions = {
+        version: '${process.env.npm_package_version}',
+        revision: '${revision}',
+        branch: '${branch}',
+        buildDate: '${moment().format('YYYYMMDD')}',
+        buildNumber: '${moment().format('HHmm')}'
+      };`;
+
+  writeFileSync(filename, content, {encoding: 'utf8'});
+}
+
+createVersionsFile('src/versions.ts').then();
